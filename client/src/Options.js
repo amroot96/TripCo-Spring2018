6443c044f18a2134f13284db376273ca9671b250
Smagisano <Smag@rams.colostate.edu>
2018-02-27 12:26:32 -0700

    Optimization button

--- client/src/Options.js
+++ client/src/Options.js
@@ -6,49 +6,74 @@ import React, {Component} from 'react';
  * The options reside in the parent object so they may be shared with the Trip object.
  * Allows the user to set the options used by the application via a set of buttons.
  */
-class Options extends Component{
-    constructor(props) {
-        super(props);
-        this.changeOption = this.changeOption.bind(this);
-        this.button1 = "btn btn-outline-dark active";
-        this.button2 = "btn btn-outline-dark";
-    }
+class Options extends Component {
+  constructor(props) {
+    super(props);
+    this.changeOption = this.changeOption.bind(this);
+    this.changeOption2 = this.changeOption2.bind(this);
+    this.button1 = "btn btn-outline-dark active";
+    this.button2 = "btn btn-outline-dark";
+    this.button3 = "btn btn-outline-dark active";
+    this.button4 = "btn btn-outline-dark";
+  }
 
-    changeOption(arg) {
-        console.log("updating distance options to...");
-        this.props.updateOptions(arg.target.id);
-        if(arg.target.id == "miles"){
-            this.button1 = "btn btn-outline-dark active";
-            this.button2 = "btn btn-outline-dark";
-        }
-        else{
-            this.button2 = "btn btn-outline-dark active";
-            this.button1 = "btn btn-outline-dark";
-        }
+  changeOption(arg) {
+    console.log("updating distance options to...");
+    this.props.updateOptions(arg.target.id,"distance");
+    if (arg.target.id == "miles") {
+      this.button1 = "btn btn-outline-dark active";
+      this.button2 = "btn btn-outline-dark";
     }
-
-
-    render() {
-        // @todo need to update the options when a button is pressed
-        return(
-            <div id="options" className="card">
-                <div className="card-header bg-success text-white">
-                    Options
-                </div>
-                <div className="card-body">
-                    <p>Highlight the options you wish to use.</p>
-                    <div className="btn-group btn-group-toggle" data-toggle="buttons">
-                        <label className={this.button1}>
-                            <input type="radio" id="miles" name="distance" autcomplete="off" onChange={this.changeOption}/> Miles
-                        </label>
-                        <label className={this.button2}>
-                            <input type="radio" id="kilometers" name="distance" autcomplete="off" onChange={this.changeOption}/> Kilometers
-                        </label>
-                    </div>
-                </div>
-            </div>
-        )
+    else {
+      this.button2 = "btn btn-outline-dark active";
+      this.button1 = "btn btn-outline-dark";
     }
+  }
+
+  changeOption2(arg) {
+    console.log("updating distance options to...");
+    this.props.updateOptions(arg.target.id,"optimization");
+    if (arg.target.id == "none") {
+      this.button3 = "btn btn-outline-dark active";
+      this.button4 = "btn btn-outline-dark";
+    }
+    else {
+      this.button4 = "btn btn-outline-dark active";
+      this.button3 = "btn btn-outline-dark";
+    }
+  }
+
+  render() {
+    // @todo need to update the options when a button is pressed
+    return (
+      <div id="options" className="card">
+        <div className="card-header bg-success text-white">
+          Options
+        </div>
+        <div className="card-body">
+          <p>Highlight the options you wish to use.</p>
+          <div className="btn-group btn-group-toggle" data-toggle="buttons">
+            <label className={this.button1}>
+              <input type="radio" id="miles" name="distance" autcomplete="off" onChange={this.changeOption}/> Miles
+            </label>
+            <label className={this.button2}>
+              <input type="radio" id="kilometers" name="distance" autcomplete="off"
+                     onChange={this.changeOption}/> Kilometers
+            </label>
+          </div>
+          <div></div>
+          <div className="btn-group btn-group-toggle" data-toggle="buttons">
+            <label className={this.button3}>
+              <input type="radio" id="none" name="optimization" autcomplete="off" onChange={this.changeOption2}/> None
+            </label>
+            <label className={this.button4}>
+              <input type="radio" id="Nearest_Neighbor" name="optimization" autcomplete="off" onChange={this.changeOption2}/> Nearest Neighbor
+            </label>
+          </div>
+        </div>
+      </div>
+    )
+  }
 }
 
 export default Options;
