30feb097cf62f1cd106513339395ebfb6834729d
Smagisano <Smag@rams.colostate.edu>
2018-03-05 09:19:01 -0700

    Don't approve yet! Made my code in Options.js pretty.

--- client/src/Options.js
+++ client/src/Options.js
@@ -1,6 +1,5 @@
 import React, {Component} from 'react';
 
-
 /* Options allows the user to change the parameters for planning
  * and rendering the trip map and itinerary.
  * The options reside in the parent object so they may be shared with the Trip object.
@@ -9,8 +8,13 @@ import React, {Component} from 'react';
 class Options extends Component {
   constructor(props) {
     super(props);
-    this.changeOption = this.changeOption.bind(this);
-    this.changeOption2 = this.changeOption2.bind(this);
+    this.kilometerButton = this.kilometerButton.bind(this);
+    this.milesButton = this.milesButton.bind(this);
+    this.nauticalButton = this.nauticalButton.bind(this);
+    this.noneButton = this.noneButton.bind(this);
+    this.shortButton = this.shortButton.bind(this);
+    this.shorterButton = this.shorterButton.bind(this);
+    this.shortestButton = this.shortestButton.bind(this);
     this.miles = "btn btn-outline-dark active";
     this.kilometers = "btn btn-outline-dark";
     this.nautical = "btn btn-outline-dark";
@@ -19,156 +23,154 @@ class Options extends Component {
     this.shorter = "btn btn-outline-dark";
     this.shortest = "btn btn-outline-dark";
   }
+
   componentWillReceiveProps(nextProps) {
-    console.log("Component will recieve props");
-    console.log(nextProps);
-    console.log(nextProps.options.distance);
-    if(nextProps.options.distance !== this.props.options.distance) {
+    if (nextProps.options.distance !== this.props.options.distance) {
       if (nextProps.options.distance === "miles") {
-        this.miles = "btn btn-outline-dark active";
-        this.kilometers = "btn btn-outline-dark";
-        this.nautical = "btn btn-outline-dark";
+        this.milesButton();
       }
       else if (nextProps.options.distance === "kilometers") {
-        this.kilometers = "btn btn-outline-dark active";
-        this.miles = "btn btn-outline-dark";
-        this.nautical = "btn btn-outline-dark";
+        this.kilometerButton();
       }
       else {
-        this.nautical = "btn btn-outline-dark active";
-        this.miles = "btn btn-outline-dark";
-        this.kilometers = "btn btn-outline-dark";
+        this.nauticalButton()
       }
     }
-    if(nextProps.options.optimization !== this.props.options.optimization) {
-      if (nextProps.options.optimization == "none") {
-        this.none = "btn btn-outline-dark active";
-        this.short = "btn btn-outline-dark";
-        this.shorter = "btn btn-outline-dark";
-        this.shortest = "btn btn-outline-dark";
+    if (nextProps.options.optimization !== this.props.options.optimization) {
+      if (nextProps.options.optimization === "none") {
+        this.noneButton();
       }
-      else if (nextProps.options.optimization == "short"){
-        this.short = "btn btn-outline-dark active";
-        this.none = "btn btn-outline-dark";
-        this.shorter = "btn btn-outline-dark";
-        this.shortest = "btn btn-outline-dark";
+      else if (nextProps.options.optimization === "short") {
+        this.shortButton();
       }
-      else if (nextProps.options.optimization == "shorter"){
-        this.shorter = "btn btn-outline-dark active";
-        this.none = "btn btn-outline-dark";
-        this.short = "btn btn-outline-dark";
-        this.shortest = "btn btn-outline-dark";
+      else if (nextProps.options.optimization === "shorter") {
+        this.shorterButton();
       }
       else {
-        this.shortest = "btn btn-outline-dark active";
-        this.none = "btn btn-outline-dark";
-        this.shorter = "btn btn-outline-dark";
-        this.short = "btn btn-outline-dark";
+        this.shortestButton();
       }
-    }
-  }
-  changeOption(arg) {
-    console.log("updating distance options to...");
-    console.log(arg.target.version);
-    this.props.updateOptions(arg.target.id,"distance");
-    if (arg.target.id == "miles") {
-      this.miles = "btn btn-outline-dark active";
-      this.kilometers = "btn btn-outline-dark";
-      this.nautical = "btn btn-outline-dark";
-    }
-    else if (arg.target.id == "kilometers") {
-      this.kilometers = "btn btn-outline-dark active";
-      this.miles = "btn btn-outline-dark";
-      this.nautical = "btn btn-outline-dark";
-    }
-    else {
-        this.nautical = "btn btn-outline-dark active";
-        this.miles = "btn btn-outline-dark";
-        this.kilometers = "btn btn-outline-dark";
     }
   }
 
-  changeOption2(arg) {
-    console.log("updating distance options to...");
-    this.props.updateOptions(arg.target.id,"optimization");
-    if (arg.target.id == "none") {
-      this.none = "btn btn-outline-dark active";
-      this.short = "btn btn-outline-dark";
-      this.shorter = "btn btn-outline-dark";
-      this.shortest = "btn btn-outline-dark";
-    }
-    else if (arg.target.id == "short"){
-      this.short = "btn btn-outline-dark active";
-      this.none = "btn btn-outline-dark";
-      this.shorter = "btn btn-outline-dark";
-      this.shortest = "btn btn-outline-dark";
-    }
-    else if (arg.target.id == "shorter"){
-      this.shorter = "btn btn-outline-dark active";
-      this.none = "btn btn-outline-dark";
-      this.short = "btn btn-outline-dark";
-      this.shortest = "btn btn-outline-dark";
-    }
-    else {
-      this.shortest = "btn btn-outline-dark active";
-      this.none = "btn btn-outline-dark";
-      this.shorter = "btn btn-outline-dark";
-      this.short = "btn btn-outline-dark";
-    }
+  shortestButton() {
+    this.props.updateOptions("shortest", "optimization");
+    this.shortest = "btn btn-outline-dark active";
+    this.none = "btn btn-outline-dark";
+    this.shorter = "btn btn-outline-dark";
+    this.short = "btn btn-outline-dark";
+  }
+
+  shorterButton() {
+    this.props.updateOptions("shorter", "optimization");
+    this.shorter = "btn btn-outline-dark active";
+    this.none = "btn btn-outline-dark";
+    this.short = "btn btn-outline-dark";
+    this.shortest = "btn btn-outline-dark";
+  }
+
+  shortButton() {
+    this.props.updateOptions("short", "optimization");
+    this.short = "btn btn-outline-dark active";
+    this.none = "btn btn-outline-dark";
+    this.shorter = "btn btn-outline-dark";
+    this.shortest = "btn btn-outline-dark";
+  }
+
+  noneButton() {
+    this.props.updateOptions("none", "optimization");
+    this.none = "btn btn-outline-dark active";
+    this.short = "btn btn-outline-dark";
+    this.shorter = "btn btn-outline-dark";
+    this.shortest = "btn btn-outline-dark";
+  }
+
+  milesButton() {
+    this.props.updateOptions("miles", "distance");
+    this.miles = "btn btn-outline-dark active";
+    this.kilometers = "btn btn-outline-dark";
+    this.nautical = "btn btn-outline-dark";
+  }
+
+  nauticalButton() {
+    this.props.updateOptions("nautical", "distance");
+    this.nautical = "btn btn-outline-dark active";
+    this.miles = "btn btn-outline-dark";
+    this.kilometers = "btn btn-outline-dark";
+  }
+
+  kilometerButton() {
+    this.props.updateOptions("kilometers", "distance");
+    this.kilometers = "btn btn-outline-dark active";
+    this.miles = "btn btn-outline-dark";
+    this.nautical = "btn btn-outline-dark";
   }
 
   render() {
-    // @todo need to update the options when a button is pressed
     return (
         <div className="container-fluid">
-            <div className="row">
-                <div className="col-xs-12 col-sm-6 col-md-6 col-lg-6 col-xl-6">
-                    <div id="options" className="card">
-                        <div className="card-header bg-success text-white">
-                            Options
-                        </div>
-                        <div className="card-body">
-                            <p>Highlight the options you wish to use.</p>
-                            <div className="btn-group btn-group-toggle" data-toggle="buttons">
-                                <label className={this.miles}>
-                                    <input type="radio" id="miles" name="distance" autoComplete="off" onChange={this.changeOption}/> Miles
-                                </label>
-                                <label className={this.kilometers}>
-                                    <input type="radio" id="kilometers" name="distance" autoComplete="off" onChange={this.changeOption}/> Kilometers
-                                </label>
-                                <label className={this.nautical}>
-                                    <input type="radio" id="nautical miles" name="distance" autoComplete="off" onChange={this.changeOption}/> Nautical Miles
-                                </label>
-                            </div>
-                            <div></div>
-                        </div>
-                    </div>
+          <div className="row">
+            <div className="col-xs-12 col-sm-6 col-md-6 col-lg-6 col-xl-6">
+              <div id="options" className="card">
+                <div className="card-header bg-success text-white">
+                  Options
                 </div>
-                <div className="col-xs-12 col-sm-6 col-md-6 col-lg-6 col-xl-6">
-                    <div id="options" className="card">
-                        <div className="card-header bg-success text-white">
-                            Optimization
-                        </div>
-                        <div className="card-body">
-                            <p>Would you like to optimize your trip? </p>
-                            <div className="btn-group btn-group-toggle" data-toggle="buttons">
-                                <label className={this.none}>
-                                    <input type="radio" id="none" name="optimization" autoComplete="off" onChange={this.changeOption2}/> No
-                                </label>
-                                <label className={this.short}>
-                                    <input type="radio" id="short" name="optimization" autoComplete="off" onChange={this.changeOption2}/> Short
-                                </label>
-                              <label className={this.shorter}>
-                                <input type="radio" id="shorter" name="optimization" autoComplete="off" onChange={this.changeOption2}/> Shorter
-                              </label>
-                              <label className={this.shortest}>
-                                <input type="radio" id="shortest" name="optimization" autoComplete="off" onChange={this.changeOption2}/> Shortest
-                              </label>
-                            </div>
-                        </div>
-                    </div>
+                <div className="card-body">
+                  <p>Highlight the options you wish to use.</p>
+                  <div className="btn-group btn-group-toggle"
+                       data-toggle="buttons">
+                    <label className={this.miles}>
+                      <input type="radio" id="miles" name="distance"
+                             autoComplete="off"
+                             onChange={this.milesButton}/> Miles
+                    </label>
+                    <label className={this.kilometers}>
+                      <input type="radio" id="kilometers" name="distance"
+                             autoComplete="off"
+                             onChange={this.kilometerButton}/> Kilometers
+                    </label>
+                    <label className={this.nautical}>
+                      <input type="radio" id="nautical miles" name="distance"
+                             autoComplete="off"
+                             onChange={this.nauticalButton}/> Nautical Miles
+                    </label>
+                  </div>
                 </div>
+              </div>
             </div>
+            <div className="col-xs-12 col-sm-6 col-md-6 col-lg-6 col-xl-6">
+              <div id="options" className="card">
+                <div className="card-header bg-success text-white">
+                  Optimization
+                </div>
+                <div className="card-body">
+                  <p>Would you like to optimize your trip? </p>
+                  <div className="btn-group btn-group-toggle"
+                       data-toggle="buttons">
+                    <label className={this.none}>
+                      <input type="radio" id="none" name="optimization"
+                             autoComplete="off"
+                             onChange={this.noneButton}/> No
+                    </label>
+                    <label className={this.short}>
+                      <input type="radio" id="short" name="optimization"
+                             autoComplete="off"
+                             onChange={this.shortButton}/> Short
+                    </label>
+                    <label className={this.shorter}>
+                      <input type="radio" id="shorter" name="optimization"
+                             autoComplete="off"
+                             onChange={this.shorterButton}/> Shorter
+                    </label>
+                    <label className={this.shortest}>
+                      <input type="radio" id="shortest" name="optimization"
+                             autoComplete="off"
+                             onChange={this.shortestButton}/> Shortest
+                    </label>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
     )
   }
