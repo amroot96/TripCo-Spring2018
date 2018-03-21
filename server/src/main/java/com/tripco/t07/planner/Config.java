8ba60963e4fda9d31e729bea3935b537dfcf0e89
Jordan <jordantp@rams.colostate.edu>
2018-03-20 22:10:35 -0600

    almost done with config rest API

---
+++ server/src/main/java/com/tripco/t07/planner/Config.java
@@ -0,0 +1,11 @@
+package com.tripco.t07.planner;
+
+
+
+public class Config  {
+
+  private final String type = "config";
+  private final int version = 2;
+  private final int optimizaiton = 2;
+
+}
