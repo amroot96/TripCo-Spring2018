32e4fc591214e7e7d1c2c5d187d2aa24d7f84476
amroot96 <amroot@rams.colostate.edu>
2018-04-17 16:22:41 -0600

    Add filter tests

--- server/src/main/java/com/tripco/t07/planner/Filter.java
+++ server/src/main/java/com/tripco/t07/planner/Filter.java
@@ -7,9 +7,13 @@ public class Filter {
     public String attribute = "";
     public ArrayList<String> values;
 
-    public String getAttr() {return this.attribute;}
+    public String getAttr() {
+        return this.attribute;
+    }
 
-    public ArrayList<String> getValues() {return this.values;}
+    public ArrayList<String> getValues() {
+        return this.values;
+    }
 
     public Filter(String att, ArrayList<String> val){
         this.attribute = att;
