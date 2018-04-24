1b6b9b6c3150796f808a8f071cc8528707ea383b
Smagisano <Smag@rams.colostate.edu>
2018-04-24 14:40:30 -0600

    Staff page simplest possible solution! Skeleton here, need to insert our info still

--- client/src/Footer.js
+++ client/src/Footer.js
@@ -6,14 +6,44 @@ import React, {Component} from 'react';
 class Footer extends Component {
     constructor(props) {
         super(props);
+      this.state = {
+        show: false,
+        resumes : null
+      };
+      this.displayStaff = this.displayStaff.bind(this);
     }
+    displayStaff(event) {
+      if(event) {
+        console.log("displaying");
+        this.setState({resumes:
+              <div className="card-body">
+            <div className="card">
+              <h3>Angela</h3>
+            </div>
+            <div className="card">
+              <h3>Courtney</h3>
+            </div>
+            <div className="card">
+              <h3>Jordan</h3>
+            </div>
+            <div className="card">
+              <h3>Scott</h3>
+            </div>
+          </div>
+        });
+      }
 
+    }
     render() {
         return (
-            <div id="footer" className="jumbotron text-white" style={{background:'#1E4D2B'}}>
+            <div id="footer">
+              <div id="footimage" className="jumbotron text-white" style={{background:'#1E4D2B'}}>
                 <h4>© TripCo t{this.props.number} {this.props.name} 2018
-                <img className={"img-fluid center"} alt="Responsive image" src="http://www.cs.colostate.edu/~cs314/images/CSU-Official-wrdmrk-357-617_Rev.png" />
+                  <img className={"img-fluid center"} alt="Responsive image" src="http://www.cs.colostate.edu/~cs314/images/CSU-Official-wrdmrk-357-617_Rev.png" />
+                  <button className="btn btn" style={{background:'#CFB53B'}} type="button" onClick={this.displayStaff}>Staff Info</button>
                 </h4>
+              </div>
+              {this.state.resumes}
             </div>
         )
     }
