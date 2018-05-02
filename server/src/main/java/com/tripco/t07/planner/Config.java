a047389a57a4d4244a3f74b13137a150a605767a
amroot96 <amroot@rams.colostate.edu>
2018-05-01 18:21:06 -0600

    I did some things. Mostly UI

--- server/src/main/java/com/tripco/t07/planner/Config.java
+++ server/src/main/java/com/tripco/t07/planner/Config.java
@@ -5,16 +5,17 @@ import java.util.ArrayList;
 public class Config  {
 
   private final String type = "config";
-  private final int version = 3;
+  private final int version = 4;
   private final Filter[] filters = {new Filter("type",new ArrayList<String>())};/*,
                                     new Filter("region",new ArrayList<String>()),
                                     new Filter("country",new ArrayList<String>()),
                                     new Filter("continent",new ArrayList<String>())};*/
   private final String[] maps = {"kml"};
-  private final int optimization = 2;
+  private final int optimization = 3;
   private final Optimizations[] optimizations = {new Optimizations("0opt","As planned"),
                                               new Optimizations("1opt","Nearest Neighbor"),
-                                              new Optimizations("2opt","2-opt algorithm")};
+                                              new Optimizations("2opt","2-opt algorithm"),
+                                              new Optimizations("3opt","3-opt algorithm")};
   private final String[] units = {"miles", "kilometers", "nautical miles", "user defined"};
 
 }
